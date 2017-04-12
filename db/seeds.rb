4ef761f1579dd1cbfa54734b6e8ff80ee7b98d97
Josh Thompson <thompsonjoshd@gmail.com>
2017-04-12 07:30:03 -0600

    prettier listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+
+Product.create!(title: "organ meat", description: %{fresh meat for the whole family},
+                image_url: "http://www.paleoista.com/wp-content/uploads/2015/10/BerkshireOrganMeat39243314x-e1444881679929-1014x487.jpg",
+                price: 49.99, )
+Product.create!(title: "eleventy sweet mobile apps", description: %{print phat stacks of cash for the whole fam, all day 'ere day.},
+                image_url: "http://www.paleoista.com/wp-content/uploads/2015/10/BerkshireOrganMeat39243314x-e1444881679929-1014x487.jpg",
+                price: 9999, )
